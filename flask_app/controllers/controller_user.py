ecae8f51bcc5de42825a754943279a3ad47bf932
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-11 17:47:51 -0700

    Registratioon Validation Complete

--- flask_app/controllers/controller_user.py
+++ flask_app/controllers/controller_user.py
@@ -6,7 +6,24 @@ from flask_app.models.model_user import User
 from flask_bcrypt import Bcrypt  #<---- Install to top of controller
 bcrypt = Bcrypt(app)
 
-
+    #  To Home Page
 @app.route('/')
 def index():
     return render_template('login.html')
+
+#     Create User through Registration Form
+@app.route('/register', methods=['POST'])
+def register():
+    if not User.validate_user(request.form):
+        return redirect('/')
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "password": pw_hash
+    }
+    user_id = User.create(data)
+    session['user_id'] = user_id
+    return redirect('/')
+
--- flask_app/models/model_user.py
+++ flask_app/models/model_user.py
@@ -7,20 +7,50 @@ import re
 from flask_bcrypt import Bcrypt        
 bcrypt = Bcrypt(app) 
 
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') #<--- Add to /model top
+
 model_db = 'private_wall'
-
-
-
 class User:
     def __init__(self,data):
         self.id = data['id']
-
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
+    @staticmethod
+    def validate_user(user):
+        is_valid = True 
+
+        # for ***Text Input*** unknown
+        if len(user['first_name']) < 2: 
+            flash("First name must be included.", category= "first_name")
+            is_valid = False 
+        if len(user['last_name']) < 2: 
+            flash("Last name must be included.", category= "last_name")
+            is_valid = False 
+        if not EMAIL_REGEX.match(user['email']): 
+            flash("Invalid email address!", category= "email")
+            is_valid = False
+        if len(user['password']) < 2: 
+            flash("A password must be included.", category= "password")
+            is_valid = False  
+        if user['password'] != user['confirm_password']: 
+            flash("Passwords must match.", category= "confirm_pw")
+            is_valid = False  
+        return is_valid
+
+
+
+
     # ***CREATE***
 
-
+    @classmethod
+    def create(cls,data):
+        query = "INSERT INTO users (first_name, last_name, email, password) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);"
+        return connectToMySQL(model_db).query_db(query, data)
 
 
 
